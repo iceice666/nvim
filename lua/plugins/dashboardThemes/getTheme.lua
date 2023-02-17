db3eb7c3c61b61dae432c5499907b0a7812e729a
iceice666 <iceice666@outlook.com>
2023-02-17 22:24:57 +0800

    update

--- lua/plugins/dashboardThemes/getTheme.lua
+++ lua/plugins/dashboardThemes/getTheme.lua
@@ -5,9 +5,16 @@
 local theme = "plugins.dashboardThemes."
 
 local themes = {
-  "default",
-  "fkunv",
-  "rickroll"
+    "default",
+    "default",
+    "default",
+    "default",
+    "default",
+    "default",
+    "default",
+    "default",
+    "fkunv",
+    "rickroll"
 }
 local t = themes[math.random(#themes)]
 math.randomseed(os.time())

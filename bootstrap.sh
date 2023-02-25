b29bbe6c9649e558d4b280650520f072097c70fe
Brian Duan <56882049+iceice666@users.noreply.github.com>
2023-02-25 22:35:39 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,6 @@
 sudo pacman -S git make gcc ripgrep fd npm lolcat
 yay -S neovim-nightly-bin
 
-cd ~/.config
-git clone https://github.com/iceice666/nvim.git
-
-cd -
 nvim
 
 
-#sudo pacman -S git make gcc ripgrep fd npm lolcat&&yay -S neovim-nightly-bin&&cd ~/.config&&git clone https://github.com/iceice666/nvim.git&&cd -&&nvim

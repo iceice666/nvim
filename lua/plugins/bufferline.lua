a0e7a58b62123ad8a0e1204d3b961f8cb645bb3b
iceice666 <iceice666@outlook.com>
2023-03-28 17:16:48 +0800

    heirline support

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,15 +8,6 @@ return {
   config = function()
     require("bufferline").setup({
       options = {
-        custom_areas = {
-          -- right = function()
-          --   local result = {}
-          --
-          --   result[#result + 1] = get_time()
-          --
-          --   return result
-          -- end,
-        },
         modified_icon = "●",
         indicator = {
           style = "icon",
@@ -27,7 +18,7 @@ return {
           local s = " "
           for e, n in pairs(diagnostics_dict) do
             local sym = e == "error" and " "
-              or (e == "warning" and " " or " ")
+                or (e == "warning" and " " or " ")
             s = s .. n .. sym
           end
           return s

7079f7806374e5098f6f32879d7bf16bf7770929
iceice666 <iceice666@outlook.com>
2023-12-13 17:07:25 +0800

    change some icon

--- lua/plugins/comment-box.lua
+++ lua/plugins/comment-box.lua
@@ -1,37 +1,37 @@
 return {
-	-- Comment box
-	"LudoPinelli/comment-box.nvim",
-	event = "BufReadPost",
-	config = function()
-		require("comment-box").setup({
-			doc_width = 60, -- width of the document
-			box_width = 60, -- width of the documentf the boxes
-			borders = { -- symbols usef the documentd to draw a box
-				top = "─",
-				bottom = "─",
-				left = "│",
-				right = "│",
-				top_left = "╭",
-				top_right = "╮",
-				bottom_left = "╰",
-				bottom_right = "╯",
-			},
-			line_width = 60, -- width f the documentof the lines
-			line = { -- symbols used tf the documento draw a line
-				line = "─",
-				line_start = "─",
-				line_end = "─",
-			},
-			outer_blank_lines = false, -- insert a blank line above and below the box
-			inner_blank_lines = false, -- insert a blank line above and below the text
-			line_blank_line_above = false, -- insert a blank line above the line
-			line_blank_line_below = false, -- insert a blank line below the line
-		})
+  -- Comment box
+  "LudoPinelli/comment-box.nvim",
+  event = "BufReadPost",
+  config = function()
+    require("comment-box").setup({
+      doc_width = 60, -- width of the document
+      box_width = 60, -- width of the documentf the boxes
+      borders = {  -- symbols usef the documentd to draw a box
+        top = "─",
+        bottom = "─",
+        left = "│",
+        right = "│",
+        top_left = "╭",
+        top_right = "╮",
+        bottom_left = "╰",
+        bottom_right = "╯",
+      },
+      line_width = 60, -- width f the documentof the lines
+      line = {      -- symbols used tf the documento draw a line
+        line = "─",
+        line_start = "─",
+        line_end = "─",
+      },
+      outer_blank_lines = false,  -- insert a blank line above and below the box
+      inner_blank_lines = false,  -- insert a blank line above and below the text
+      line_blank_line_above = false, -- insert a blank line above the line
+      line_blank_line_below = false, -- insert a blank line below the line
+    })
 
-		local mapx = vim.g.mapx
+    local mapx = vim.g.mapx
 
-		mapx.group({ silent = true }, function()
-			mapx.vnoremap("<leader>bc", "<cmd>CBccbox<cr>")
-		end)
-	end,
+    mapx.group({ silent = true }, function()
+      mapx.vnoremap("<leader>bc", "<cmd>CBccbox<cr>")
+    end)
+  end,
 }

b8d77ca665639a4efccd7845180a5767d58c40f6
iceice666 <iceice666@outlook.com>
2024-01-05 00:43:06 +0800

    change `HACKY` icon

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -3,55 +3,59 @@ return {
   "folke/todo-comments.nvim",
   event = "BufReadPost",
   opts = {
-      keywords = {
-        IMPORTANT = {
-          icon = "󰀨 ",
-          color = "error",
-          alt = {
-            "NEED_FIX",
-            "FIXME",
-            "BUG",
-            "FIXIT",
-            "ISSUE",
-            "IMPORTANT",
-            "!!!!!",
-          },
-        },
-        TODO = {
-          icon = " ",
-          color = "todo",
-          alt = { "TODO" },
-        },
-        HACK = {
-          icon = " ",
-          color = "warning",
-          alt = { "HACK", "HACKY" },
-        },
-        WARN = {
-          icon = " ",
-          color = "warning",
-          alt = { "WARNING", "WARN" },
-        },
-        NOTE = {
-          icon = " ",
-          color = "hint",
-          alt = { "INFO", "NOTE" },
-        },
-        TEST = {
-          icon = "⏲ ",
-          color = "test",
-          alt = { "TESTING", "PASSED", "FAILED" },
+    keywords = {
+      IMPORTANT = {
+        icon = "󰀨 ",
+        color = "error",
+        alt = {
+          "NEED_FIX",
+          "FIXME",
+          "BUG",
+          "FIXIT",
+          "ISSUE",
+          "IMPORTANT",
+          "!!!",
         },
       },
-      colors = {
-        todo = { "#ccddff" },
-        error = { "#DC2626" },
-        warning = { "#FBBF24" },
-        hint = { "#ffb9ff" },
-        test = { "#FF00FF" },
+      TODO = {
+        icon = " ",
+        color = "todo",
+        alt = { "TODO" },
+      },
+      HACK = {
+        icon = [[🤔]],
+        color = "warning",
+        alt = { "HACK", "HACKY" },
+      },
+      WARN = {
+        icon = " ",
+        color = "warning",
+        alt = { "WARNING", "WARN" },
+      },
+      NOTE = {
+        icon = " ",
+        color = "hint",
+        alt = { "INFO", "NOTE" },
+      },
+      TEST = {
+        icon = "⏲ ",
+        color = "test",
+        alt = { "TESTING", "PASSED", "FAILED" },
       },
     },
+    colors = {
+      todo = { "#ccddff" },
+      error = { "#DC2626" },
+      warning = { "#FBBF24" },
+      hint = { "#ffb9ff" },
+      test = { "#FF00FF" },
+    },
+  },
   keys = {
-    { "<leader>sT", "<cmd>TodoTrouble<cr>", desc = "Trouble: Show todos" },
+    {
+      "<leader>sT",
+      "<cmd>TodoTrouble keywords=TODO<cr>",
+      desc = "Trouble: Show todos",
+    },
   },
 }

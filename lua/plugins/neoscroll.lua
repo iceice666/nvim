861a6be0938d5bd58f626b5dc0ea051a294b7367
iceice666 <iceice666@outlook.com>
2024-01-10 17:17:32 +0800

    feat(scroll): add neoscroll animate for other scroll like <c-U>

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -19,6 +19,60 @@ return {
       mode = { "n", "x", "o" },
       desc = "Up 10 lines",
     },
+    {
+      "<C-u>",
+      function()
+        require("neoscroll").scroll(-vim.wo.scroll, true, 250)
+      end,
+    },
+    {
+      "<C-d>",
+      function()
+        require("neoscroll").scroll(vim.wo.scroll, true, 250)
+      end,
+    },
+    {
+      "<C-b>",
+      function()
+        require("neoscroll").scroll(-vim.api.nvim_win_get_height(0), true, 450)
+      end,
+    },
+    {
+      "<C-f>",
+      function()
+        require("neoscroll").scroll(vim.api.nvim_win_get_height(0), true, 450)
+      end,
+    },
+    {
+      "<C-y>",
+      function()
+        require("neoscroll").scroll(-0.10, false, 100)
+      end,
+    },
+    {
+      "<C-e>",
+      function()
+        require("neoscroll").scroll(0.10, false, 100)
+      end,
+    },
+    {
+      "zt",
+      function()
+        require("neoscroll").zt("250")
+      end,
+    },
+    {
+      "zz",
+      function()
+        require("neoscroll").zz("250")
+      end,
+    },
+    {
+      "zb",
+      function()
+        require("neoscroll").zb("250")
+      end,
+    },
   },
   opts = {
     mappings = {},

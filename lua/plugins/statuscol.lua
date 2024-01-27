354f8608564dfd31305382fc7a141b87b66fbeb4
iceice666 <iceice666@outlook.com>
2024-01-28 02:33:44 +0800

    update

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -1,15 +1,18 @@
 return {
   -- statuscolumn
-  -- TODO: Need config
   "luukvbaal/statuscol.nvim",
   event = "BufReadPost",
   config = function()
     local builtin = require("statuscol.builtin")
+
     require("statuscol").setup({
       relculright = true,
       segments = {
-        { text = { "%C" }, click = "v:lua.ScFa" },
         { text = { "%s" }, click = "v:lua.ScSa" },
+        {
+          text = { builtin.foldfunc },
+          click = "v:lua.ScFa",
+        },
         {
           text = { builtin.lnumfunc, " " },
           condition = { true, builtin.not_empty },
--- lua/plugins/statusline/heirline/git.lua
+++ lua/plugins/statusline/heirline/git.lua
@@ -3,13 +3,10 @@ return {
   condition = conditions.is_git_repo,
   init = function(self)
     self.status_dict = vim.b.gitsigns_status_dict
-    self.has_changes = self.status_dict.added ~= 0
-      or self.status_dict.removed ~= 0
-      or self.status_dict.changed ~= 0
   end,
   on_click = {
     callback = function()
-      vim.cmd("Neogit")
+      vim.cmd("LazyGit")
     end,
     name = "heirline_git",
   },

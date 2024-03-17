9955a497606f744096b0b4172b397197d6268afb
iceice666 <iceice666@outlook.com>
2024-03-17 14:56:29 +0800

    change some description of hydra buffer

--- lua/plugins/hydra/buffer.lua
+++ lua/plugins/hydra/buffer.lua
@@ -5,12 +5,12 @@ local cmd = require("hydra.keymap-util").cmd
 local hint = [[
  Buffer
 
-      close      ^^^  Move        Goto     ^^^^     Sort by    
+      Close      ^^^  Move        Goto     ^^^^     Sort by    
  --------------- ^^^--------- ------------ ^^^^----------------------- 
  _q_: current       _>_: next ^ _]_: next       ^_e_: extension
  _p_: picked one    _<_: prev ^ _[_: prev       ^_d_: directory
  _a_: all                   ^^ _\^_: first      ^_r_: relative directory 
- _o_: remain only           ^^^ _$_: last       ^_t_: tabs
+ _o_: others                ^^^ _$_: last       ^_t_: tabs
                            ^^^^^_s_: selected
  _<Esc>_: quit                             ^^^^^^_P_: toggle pin
 ]]

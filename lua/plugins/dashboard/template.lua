1a3c3d47c52388759543cf6672d7bbd078c5b1a2
iceice666 <iceice666@outlook.com>
2024-03-20 21:06:06 +0800

    fmt

--- lua/plugins/dashboard/template.lua
+++ lua/plugins/dashboard/template.lua
@@ -1,83 +1,91 @@
 local center = function()
-	local buttons = {}
-	local button = function(shortcut, icon, label, action, hl)
-		shortcut = shortcut or nil
-		icon = icon or nil
-		label = label or nil
-		action = action or nil
-		hl = hl or "DashboardShotCut"
-		buttons[#buttons + 1] = {
-			desc = icon .. "  " .. label .. string.rep(" ", 47 - #label),
-			desc_hl = "DashboardDesc",
-			action = action,
-			key = shortcut,
-			key_hl = "DashboardKey",
-		}
-	end
+  local buttons = {}
+  local button = function(shortcut, icon, label, action, hl)
+    shortcut = shortcut or nil
+    icon = icon or nil
+    label = label or nil
+    action = action or nil
+    hl = hl or "DashboardShotCut"
+    buttons[#buttons + 1] = {
+      desc = icon .. "  " .. label .. string.rep(" ", 47 - #label),
+      desc_hl = "DashboardDesc",
+      action = action,
+      key = shortcut,
+      key_hl = "DashboardKey",
+    }
+  end
 
-	button("r", "󰉋 ", "Restore Session", "SessionRestore")
-	button("c", "󱎘 ", "Close dashboard", "enew ")
-	button("m", "󰍦 ", "Messages", "Noice")
-	button("p", " ", "Lazy profile", "Lazy profile")
-	button("R", "󰑓 ", "Restart nvim", "cq")
-	button("q", "󰗼 ", "Quit", "qa")
+  button("r", "󰉋 ", "Restore Session", "SessionRestore")
+  button("c", "󱎘 ", "Close dashboard", "enew ")
+  button("m", "󰍦 ", "Messages", "Noice")
+  button("p", " ", "Lazy profile", "Lazy profile")
+  button("R", "󰑓 ", "Restart nvim", "cq")
+  button("q", "󰗼 ", "Quit", "qa")
 
-	return buttons
+  return buttons
 end
 
 local array_combine = function(table, text)
-	table = table or {}
-	text = text or {}
+  table = table or {}
+  text = text or {}
 
-	if type(text) == "string" then
-		text = vim.split(text, "\n", { trimempty = true })
-	elseif type(text) ~= "table" then
-		return table
-	end
+  if type(text) == "string" then
+    text = vim.split(text, "\n", { trimempty = true })
+  elseif type(text) ~= "table" then
+    return table
+  end
 
-	for _, k in ipairs(text) do
-		table[#table + 1] = k
-	end
+  for _, k in ipairs(text) do
+    table[#table + 1] = k
+  end
 
-	return table
+  return table
 end
 
 local header = function(text)
-	local header = array_combine({}, text)
+  local header = array_combine({}, text)
 
-	return header
+  return header
 end
 
 local footer = function(text)
-	local version = vim.version()
-	if version ~= nil then
-		version = "Neovim v" .. version.major .. "." .. version.minor .. "." .. version.patch
-	end
+  local version = vim.version()
+  if version ~= nil then
+    version = "Neovim v"
+      .. version.major
+      .. "."
+      .. version.minor
+      .. "."
+      .. version.patch
+  end
 
-	local stats = require("lazy").stats()
-	local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+  local stats = require("lazy").stats()
+  local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
 
-	local footer = {
-		"",
-		version .. " started in " .. ms .. "ms",
-		"loaded: " .. stats.loaded .. ", lazy-loaded: " .. stats.count - stats.loaded,
-		"",
-	}
+  local footer = {
+    "",
+    version .. " started in " .. ms .. "ms",
+    "loaded: "
+      .. stats.loaded
+      .. ", lazy-loaded: "
+      .. stats.count - stats.loaded,
+    "",
+  }
 
-	return array_combine(footer, text)
+  return array_combine(footer, text)
 end
 
 return {
-	theme = "doom",
-	config = {
-		packages = { enable = false },
-		shortcut = center(),
-		center = center(),
-		header = header,
-		footer = footer,
-	},
-	hide = {
-		tabline = true,
-		statusline = true,
-	},
+  theme = "doom",
+  config = {
+    packages = { enable = false },
+    shortcut = center(),
+    center = center(),
+    header = header,
+    footer = footer,
+  },
+  hide = {
+    tabline = true,
+    statusline = true,
+  },
 }
--- lua/plugins/hydra/buffer.lua
+++ lua/plugins/hydra/buffer.lua
@@ -7,10 +7,10 @@ local hint = [[
 
       Close      ^^^  Move        Goto     ^^^^     Sort by    
  --------------- ^^^--------- ------------ ^^^^----------------------- 
- _q_: current       _>_: next ^ _]_: next       ^_e_: extension
- _p_: picked one    _<_: prev ^ _[_: prev       ^_d_: directory
- _a_: all                   ^^ _\^_: first      ^_r_: relative directory 
- _o_: others                ^^^ _$_: last       ^_t_: tabs
+  _p_: picked one   _>_: next ^ _]_: next       ^_e_: extension
+  _a_: all          _<_: prev ^ _[_: prev       ^_d_: directory
+  _o_: others               ^^ _\^_: first      ^_r_: relative directory 
+                          ^^^^^ _$_: last       ^_t_: tabs
                            ^^^^^_s_: selected
  _<Esc>_: quit                             ^^^^^^_P_: toggle pin
 ]]
@@ -35,7 +35,6 @@ Hydra({
   heads = {
     { "s", cmd("BufferLinePick") },
     { "P", cmd("BufferLineTogglePin") },
-    { "q", cmd("bd %") },
     { "o", cmd("BufferLineCloseOthers"), { exit = true } },
     { "a", cmd("BufferLineCloseOthers") .. cmd("bd%"), { exit = true } },
     { "p", cmd("BufferLinePickClose") },

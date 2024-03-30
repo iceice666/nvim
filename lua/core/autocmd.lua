c0cb32d43cd937eaad576c7fba6eda182d627e03
iceice666 <iceice666@outlook.com>
2024-03-30 23:52:26 +0800

    remove unnecessary autocmd

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,14 +1,3 @@
-vim.api.nvim_create_autocmd({ "VimLeave" }, {
-  callback = function()
-    pcall(require("dapui").close, "")
-    pcall(vim.api.nvim_exec, "UndotreeHide")
-    pcall(vim.api.nvim_exec, "NeoTreeClose")
-    pcall(vim.api.nvim_exec, "TroubleClose")
-
-    pcall(vim.api.nvim_exec, "SessionSave")
-  end,
-})
-
 vim.api.nvim_create_autocmd("BufWinEnter", {
   pattern = "*",
   callback = function(event)
@@ -18,26 +7,3 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
     end
   end,
 })
-
-vim.api.nvim_create_autocmd("BufWinEnter", {
-  pattern = "*",
-  callback = function(event)
-    if vim.bo[event.buf].filetype == "term" then
-      vim.bo[event.buf].modifiable = true
-    end
-  end,
-})
--- vim.api.nvim_create_autocmd({ "TextYankPost" }, {
---   callback = function()
---     vim.highlight.on_yank({
---       higroup = "IncSearch",
---       timeout = 300,
---     })
---
---     if vim.fn.has("wsl") == 1 then
---       vim.fn.system("clip.exe", vim.fn.getreg("\""))
---     end
---   end,
---
---   group = vim.api.nvim_create_augroup("Yank", { clear = true }),
--- })

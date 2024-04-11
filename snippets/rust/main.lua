a99ccd2d723a197b90645c752c639e17bc75e6e5
iceice666 <iceice666@outlook.com>
2024-04-11 19:30:06 +0800

    add rust tokio::test snippet

--- snippets/rust/main.lua
+++ snippets/rust/main.lua
@@ -1,59 +1,90 @@
-
 return {
 
-  s('anyresult', t 'type AnyResult<T = ()> = anyhow::Result<T>;'),
-  s('returnanyresult', t ' -> AnyResult'),
+  s("anyresult", t("type AnyResult<T = ()> = anyhow::Result<T>;")),
+  s("returnanyresult", t(" -> AnyResult")),
 
-  s('derivedebug', t '#[derive(Debug)]'),
-  s('deadcode', t '#[allow(dead_code)]'),
-  s('allowfreedom', t '#![allow(clippy::disallowed_names, unused_variables, dead_code)]'),
+  s("derivedebug", t("#[derive(Debug)]")),
+  s("deadcode", t("#[allow(dead_code)]")),
+  s(
+    "allowfreedom",
+    t("#![allow(clippy::disallowed_names, unused_variables, dead_code)]")
+  ),
 
-  s('clippypedantic', t '#![warn(clippy::all, clippy::pedantic)]'),
+  s("clippypedantic", t("#![warn(clippy::all, clippy::pedantic)]")),
 
-  s(':turbofish', { t {'::<'}, i(0), t {'>'} }),
+  s(":turbofish", { t({ "::<" }), i(0), t({ ">" }) }),
 
-  s('print', {
+  s("print", {
     -- t {'println!("'}, i(1), t {' {:?}", '}, i(0), t {');'}}),
-    t {'println!("'}, i(1), t {' {'}, i(0), t {':?}");'}}),
-
-  s('for',
-  {
-  t {'for '}, i(1), t {' in ' }, i(2), t {' {', ''},
-        i(0),
-  t {'}', ''},
+    t({ "println!(\"" }),
+    i(1),
+    t({ " {" }),
+    i(0),
+    t({ ":?}\");" }),
   }),
 
-  s('struct',
-  {
-    t {'#[derive(Debug)]', ''},
-    t {'struct '}, i(1), t {' {', ''},
-      i(0),
-    t {'}', ''},
+  s("for", {
+    t({ "for " }),
+    i(1),
+    t({ " in " }),
+    i(2),
+    t({ " {", "" }),
+    i(0),
+    t({ "}", "" }),
   }),
 
-  s('test',
-  {
-      t {'#[test]', ''},
-      t {'fn '}, i(1,'test'), t {'() {', ''},
-      t {'	assert'}, i(0), t {'', ''},
-      t {'}'},
+  s("struct", {
+    t({ "#[derive(Debug)]", "" }),
+    t({ "struct " }),
+    i(1),
+    t({ " {", "" }),
+    i(0),
+    t({ "}", "" }),
   }),
 
-  s('testcfg',
-  {
-    t {'#[cfg(test)]', ''},
-    t {'mod '}, i(1, 'test'), t {' {', ''},
-    t {'	#[test]', ''},
-    t {'	fn '}, i(2, 'test'), t {'() {', ''},
-    t {'		assert'}, i(0), t {'', ''},
-    t {'	}', ''},
-    t {'}'},
+  s("test", {
+    t({ "#[test]", "" }),
+    t({ "fn " }),
+    i(1, "test"),
+    t({ "() {", "" }),
+    t({ "	assert" }),
+    i(0),
+    t({ "", "" }),
+    t({ "}" }),
   }),
 
-  s('if',
-  {
-    t {'if '}, i(1), t {' {', ''},
-     i(0),
-    t {'}'},
+  s("asynctest", {
+    t({ "#[tokio::test]", "" }),
+    t({ "fn " }),
+    i(1, "test"),
+    t({ "() {", "" }),
+    t({ "	assert" }),
+    i(0),
+    t({ "", "" }),
+    t({ "}" }),
+  }),
+
+  s("testcfg", {
+    t({ "#[cfg(test)]", "" }),
+    t({ "mod " }),
+    i(1, "test"),
+    t({ " {", "" }),
+    t({ "	#[test]", "" }),
+    t({ "	fn " }),
+    i(2, "test"),
+    t({ "() {", "" }),
+    t({ "		assert" }),
+    i(0),
+    t({ "", "" }),
+    t({ "	}", "" }),
+    t({ "}" }),
+  }),
+
+  s("if", {
+    t({ "if " }),
+    i(1),
+    t({ " {", "" }),
+    i(0),
+    t({ "}" }),
   }),
 }

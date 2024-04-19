4ba7a63d6b7c5aec0045cd4d2f46686bdf682636
iceice666 <iceice666@outlook.com>
2024-04-19 13:52:40 +0800

    disable check for typescript

--- lua/plugins/lsp/server/volar.lua
+++ lua/plugins/lsp/server/volar.lua
@@ -8,28 +8,26 @@ local lspconfig = require("lspconfig")
 
 local lsp_default = require("plugins.lsp.server._default")
 
-if vim.g.util.is_npm_package_installed("typescript") then
-  lspconfig.tsserver.setup({
-    capabilities = lsp_default.capabilities(),
-    on_attach = lsp_default.on_attach,
-    init_options = {
-      plugins = {
-        {
-          name = "@vue/typescript-plugin",
-          location = vue_language_server_path,
-          languages = { "vue" },
-        },
+lspconfig.tsserver.setup({
+  capabilities = lsp_default.capabilities(),
+  on_attach = lsp_default.on_attach,
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_path,
+        languages = { "vue" },
       },
     },
-    filetypes = {
-      "typescript",
-      "javascript",
-      "javascriptreact",
-      "typescriptreact",
-      "vue",
-    },
-  })
-end
+  },
+  filetypes = {
+    "typescript",
+    "javascript",
+    "javascriptreact",
+    "typescriptreact",
+    "vue",
+  },
+})
 
 -- No need to set `hybridMode` to `true` as it's the default value
 lspconfig.volar.setup({

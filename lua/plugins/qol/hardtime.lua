97e62020d2b3c55c76b24a856c8611ed64662b32
iceice666 <iceice666@outlook.com>
2024-05-29 03:03:09 +0800

    feat: hardtime.nvim

---
+++ lua/plugins/qol/hardtime.lua
@@ -0,0 +1,21 @@
+return
+{
+  event = "VeryLazy",
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts = {
+    disabled_filetypes = { "neo-tree", "netrw", "lazy", "mason", "oil" },
+    disabled_keys = {
+    },
+    disable_mouse = true,
+    max_count = 5,
+    hints = {
+      ["[dcyvV][ia][%(%)]"] = {
+        message = function(keys)
+          return "Use " .. keys:sub(1, 2) .. "b instead of " .. keys
+        end,
+        length = 3,
+      },
+    },
+  }
+}

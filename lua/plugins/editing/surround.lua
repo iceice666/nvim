e4a1040e4e3b9da84cdebc2414a034736801d4d1
iceice666 <iceice666@outlook.com>
2024-05-29 03:02:52 +0800

    feat(surround): add which-key support for surround.vim

--- lua/plugins/editing/surround.lua
+++ lua/plugins/editing/surround.lua
@@ -1,6 +1,13 @@
 return {
-  "kylechui/nvim-surround",
-  version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  event = "BufReadPost",
-  config = true,
+  "roobert/surround-ui.nvim",
+  dependencies = {
+    {
+      "kylechui/nvim-surround",
+      config = true
+    },
+    "folke/which-key.nvim",
+  },
+  opts         = {
+    root_key = "S"
+  }
 }

9dd06aaa5c08062f2c57026b41e7c27f5c44fe25
iceice666 <iceice666@outlook.com>
2024-05-30 00:05:41 +0800

    enable arrow key in insert mode

--- lua/plugins/qol/hardtime.lua
+++ lua/plugins/qol/hardtime.lua
@@ -1,11 +1,14 @@
-return
-{
+return {
   event = "VeryLazy",
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
   opts = {
     disabled_filetypes = { "spectre_panel", "noice", "neo-tree", "netrw", "lazy", "mason", "oil" },
     disabled_keys = {
+      ["<Up>"] = { "", },
+      ["<Down>"] = { "", },
+      ["<Left>"] = { "", },
+      ["<Right>"] = { "", },
     },
     disable_mouse = true,
     max_count = 5,

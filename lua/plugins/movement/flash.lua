1a13596c4bc5b447f04e1c54e265061fab14fbb8
iceice666 <iceice666@outlook.com>
2024-05-30 01:25:17 +0800

    change flash.nvim keybinding

--- lua/plugins/movement/flash.lua
+++ lua/plugins/movement/flash.lua
@@ -10,10 +10,10 @@ return {
   },
   -- stylua: ignore start
   keys = {
-    { "\\", mode = { "n", "x", "o" }, function() require("flash").jump() end,             desc = "Flash" },
-    { "|", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,       desc = "Flash Treesitter" },
-    { "r", mode = "o",              function() require("flash").remote() end,            desc = "Remote Flash" },
-    { "R", mode = { "o", "x" },     function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    { "<m-s>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    { "r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+    { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
   },
   -- stylua: ignore end
 }

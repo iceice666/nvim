d31864096b168a8035931af08d4fad3a2cb5e365
iceice666 <iceice666@outlook.com>
2024-05-30 00:51:41 +0800

    vim.ui adjustment

--- lua/plugins/ui/dressing.lua
+++ lua/plugins/ui/dressing.lua
@@ -1,5 +1,13 @@
 return {
   'stevearc/dressing.nvim',
-  lazy = true,
-  opts = {},
+  lazy = false,
+  opts = {
+    input = {
+      override = function(conf)
+        conf.col = -1
+        conf.row = 0
+        return conf
+      end,
+    },
+  },
 }

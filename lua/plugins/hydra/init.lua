309c2233068e39efac3d0af8ea10e5dabee103b2
iceice666 <iceice666@outlook.com>
2024-05-30 00:06:33 +0800

    refactor(hydra): improve loading speed

--- lua/plugins/hydra/init.lua
+++ lua/plugins/hydra/init.lua
@@ -1,67 +1,52 @@
 return {
-  "nvimtools/hydra.nvim",
-  -- event = "VeryLazy",
-  dependencies = {
-    -- window management
-    "mrjones2014/smart-splits.nvim",
-    "sindrets/winshift.nvim",
-    {
-      "anuvyklack/windows.nvim",
-      dependencies = {
-        "anuvyklack/middleclass",
-        "anuvyklack/animation.nvim",
-      },
-      config = true,
-    },
-
-    -- telescope
-    "nvim-telescope/telescope.nvim",
-
-    -- for fun
-    "giusgad/pets.nvim",
-    "tamton-aquib/duck.nvim",
-  },
-  config = function()
-    local req = function(text)
-      return require("plugins.hydra.components." .. text)
-    end
-
-    local is_lsp_active = function(lsp_name)
-      for _, client in ipairs(vim.lsp.get_active_clients()) do
-        if client.name == lsp_name then
-          return true
-        end
-      end
-      print("false!")
-
-      return false
-    end
-
-    req("tabpage")
-    req("telescope")
-    req("winctrl")
-    req("for_fun")
-    req("buffer")
-
-    local is_file_exist = function(path)
-      return vim.fn.filereadable(vim.fn.getcwd() .. "/" .. path) == 1
-    end
-
-    require("which-key").register({
-      ["<leader>l"] = {
+  {
+    "nvimtools/hydra.nvim",
+    lazy = false,
+    config = function()
+      -- register components after vim loaded
+      vim.g.utils.require_hydra("tabpage")
+      vim.g.utils.require_hydra("telescope")
+      vim.g.utils.require_hydra("winctrl")
+      -- vim.g.utils.require_hydra("for_fun")
+      vim.g.utils.require_hydra("buffer")
+    end,
+    keys = {
+      {
+        "<leader>l",
         function()
-          local km = req("lsp.default")
+          local km = vim.g.utils.require_hydra("lsp.default")
 
-          if is_file_exist("Cargo.toml") or is_lsp_active("rust_analyzer") then
-            km = req("lsp.rust")
-          elseif is_file_exist("pubspec.yaml") or is_lsp_active("dartls") then
-            km = req("lsp.flutter")
+          if vim.g.utils.is_file_exist("Cargo.toml") or vim.g.utils.is_lsp_active("rust_analyzer") then
+            km = vim.g.utils.require_hydra("lsp.rust")
+          elseif vim.g.utils.is_file_exist("pubspec.yaml") or vim.g.utils.is_lsp_active("dartls") then
+            km = vim.g.utils.require_hydra("lsp.flutter")
           end
 
           km:activate()
         end,
-        "[Hydra] Lsp binding",
+        desc = "[Hydra] Lsp binding",
       },
-    })
-  end,
+    }
+
+  },
+  {
+    "mrjones2014/smart-splits.nvim",
+    event = "VeryLazy",
+    lazy = true
+  },
+  {
+    "sindrets/winshift.nvim",
+    event = "VeryLazy",
+    lazy = true,
+
+  },
+  {
+    "anuvyklack/windows.nvim",
+    lazy = true,
+    dependencies = {
+      "anuvyklack/middleclass",
+      "anuvyklack/animation.nvim",
+    },
+    config = true,
+  }
 }

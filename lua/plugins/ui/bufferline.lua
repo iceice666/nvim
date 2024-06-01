b8ad3adb5827d1f95d2ec2ee4e19a5a2f401b2f4
iceice666 <iceice666@outlook.com>
2024-06-01 22:12:59 +0800

    update buffernvim

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -12,7 +12,7 @@ return {
         local s = " "
         for e, n in pairs(diagnostics_dict) do
           local sym = e == "error" and " "
-            or (e == "warning" and " " or " ")
+              or (e == "warning" and " " or " ")
           s = s .. n .. sym
         end
         return s
@@ -34,23 +34,27 @@ return {
     },
   },
   keys = {
-   -- stylua: ignore start
-    { "<leader>[",  "<cmd>BufferLineCyclePrev<cr>",                 desc = "Buf: Switch to prev buffer",    },
-    { "<leader>]",  "<cmd>BufferLineCycleNext<cr>",                 desc = "Buf: Switch to next buffer",    },
-    { "<leader>1",  "<cmd>BufferLineGoToBuffer 1<CR>",              desc = "Buf: Switch to buffer1",        },
-    { "<leader>2",  "<cmd>BufferLineGoToBuffer 2<CR>",              desc = "Buf: Switch to buffer2",        },
-    { "<leader>3",  "<cmd>BufferLineGoToBuffer 3<CR>",              desc = "Buf: Switch to buffer3",        },
-    { "<leader>4",  "<cmd>BufferLineGoToBuffer 4<CR>",              desc = "Buf: Switch to buffer4",        },
-    { "<leader>5",  "<cmd>BufferLineGoToBuffer 5<CR>",              desc = "Buf: Switch to buffer5",        },
-    { "<leader>6",  "<cmd>BufferLineGoToBuffer 6<CR>",              desc = "Buf: Switch to buffer6",        },
-    { "<leader>7",  "<cmd>BufferLineGoToBuffer 7<CR>",              desc = "Buf: Switch to buffer7",        },
-    { "<leader>8",  "<cmd>BufferLineGoToBuffer 8<CR>",              desc = "Buf: Switch to buffer8",        },
-    { "<leader>9",  "<cmd>BufferLineGoToBuffer 9<CR>",              desc = "Buf: Switch to buffer9",        },
-    { "<leader>q",function ()
-      local ok,_ = pcall(vim.cmd, "Bdelete")
-      if not ok then
-        vim.cmd("bd")
-      end
-    end,          desc = "Buf: Close current buffer",     },
+    -- stylua: ignore start
+    { "[b",        "<cmd>BufferLineCyclePrev<cr>",    desc = "Buf: Switch to prev buffer", },
+    { "]b",        "<cmd>BufferLineCycleNext<cr>",    desc = "Buf: Switch to next buffer", },
+    { "<leader>1", "<cmd>BufferLineGoToBuffer 1<CR>", desc = "Buf: Switch to buffer1", },
+    { "<leader>2", "<cmd>BufferLineGoToBuffer 2<CR>", desc = "Buf: Switch to buffer2", },
+    { "<leader>3", "<cmd>BufferLineGoToBuffer 3<CR>", desc = "Buf: Switch to buffer3", },
+    { "<leader>4", "<cmd>BufferLineGoToBuffer 4<CR>", desc = "Buf: Switch to buffer4", },
+    { "<leader>5", "<cmd>BufferLineGoToBuffer 5<CR>", desc = "Buf: Switch to buffer5", },
+    { "<leader>6", "<cmd>BufferLineGoToBuffer 6<CR>", desc = "Buf: Switch to buffer6", },
+    { "<leader>7", "<cmd>BufferLineGoToBuffer 7<CR>", desc = "Buf: Switch to buffer7", },
+    { "<leader>8", "<cmd>BufferLineGoToBuffer 8<CR>", desc = "Buf: Switch to buffer8", },
+    { "<leader>9", "<cmd>BufferLineGoToBuffer 9<CR>", desc = "Buf: Switch to buffer9", },
+    {
+      "<leader>q",
+      function()
+        local ok, _ = pcall(vim.cmd, "Bdelete")
+        if not ok then
+          vim.cmd("bd")
+        end
+      end,
+      desc = "Buf: Close current buffer",
+    },
   },
 }

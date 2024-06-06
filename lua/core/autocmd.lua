9e2896e75811cc4b1cec3da6adc93c83bd92d59f
iceice666 <iceice666@outlook.com>
2024-06-06 21:14:54 +0800

    update

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,9 +1,47 @@
+local user_autocmd = vim.api.nvim_create_augroup("UserAutoCmd", { clear = true })
+
 -- Open help file in a individual window
 vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = user_autocmd,
   callback = function(event)
     if vim.bo[event.buf].filetype == "help" then
       vim.bo[event.buf].buflisted = true
       vim.cmd.only()
     end
   end,
+})
+
+
+-- yank highlight
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = user_autocmd,
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end,
+})
+
+
+-- auto save
+vim.api.nvim_create_autocmd({ "InsertLeave", "CursorHold" }, {
+  pattern = "*",
+  group = user_autocmd,
+  desc = 'Auto save when leaving insert mode or doesnt press a key for a long time',
+  callback = function()
+    if vim.fn.filewritable(vim.fn.expand("%")) == 1 or
+        vim.fn.findfile(vim.fn.expand("%:t"), vim.fn.expand("%:h")) == 0 then
+      vim.cmd("w")
+    end
+  end
+})
+
+-- update when focus gained
+vim.api.nvim_create_autocmd("FocusGained", {
+  pattern = "*",
+  group = user_autocmd,
+  desc = 'Update when focus gained',
+  callback = function()
+    vim.cmd("checktime")
+  end
 })
--- lua/core/vscode.lua
+++ lua/core/vscode.lua
@@ -1,6 +1,17 @@
 if vim.g.vscode ~= 1 then return end
 
-vim.notify = require("vscode").notify
+local vscode = require("vscode")
+
+vim.notify = vscode.notify
 vim.g.clipboard = vim.g.vscode_clipboard
 
 vim.notify("Load vscode config!")
+
+local action = vscode.action
+
+-- TODO:
+local km = {
+  { "<leader>e", action("workbench.view.explorer") }
+}
+
+vim.g.utils.set_keymap(km)

5ec13735d8248b0857f3e97e53769c1795ad5fec
iceice666 <iceice666@outlook.com>
2024-06-09 14:47:14 +0800

    deprecate dap
    bcuz i dont mainly write python anymore.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,4 @@
-return {
+local enable = {
   require("plugins.theme.init"),                                -- theme
   require("plugins.dashboard.init"),                            -- Welcome screen
   { import = "plugins.ui",       enabled = 1 ~= vim.g.vscode }, -- Modify Neovim built-in UI
@@ -10,7 +10,12 @@ return {
   { import = "plugins.cmp",      enabled = 1 ~= vim.g.vscode }, -- Auto-completion
   { import = "plugins.heirline", enabled = 1 ~= vim.g.vscode }, -- statusline
   { import = "plugins.hydra",    enabled = 1 ~= vim.g.vscode }, -- Custom submodes and menus (like Emacs hydra)
-  { import = "plugins.dap",      enabled = 1 ~= vim.g.vscode }, -- Debug Adapter Protocol
   { import = "plugins.fun",      enabled = 1 ~= vim.g.vscode }, -- Just for fun
 }
 
+-- Deprecated
+-- { import = "plugins.dap",      enabled = 1 ~= vim.g.vscode }, -- Debug Adapter Protocol
+
+
+
+return enable

afd34b7717152fd418a00bee8c7dee8098f0c7a9
iceice666 <iceice666@outlook.com>
2024-08-11 04:45:20 +0800

    yanky replace smart-yank

--- lua/plugins/lsp/server/_default.lua
+++ lua/plugins/lsp/server/_default.lua
@@ -13,6 +13,7 @@ function M.on_attach(client, bufnr)
   if client.supports_method("textDocument/inlayHint", { bufnr = bufnr }) then
     vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
   end
+  require("copilot_cmp")._on_insert_enter({})
 end
 
 return M

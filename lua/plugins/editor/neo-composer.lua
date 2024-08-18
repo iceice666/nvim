578fa894d34a8de8e468fc99afe107748da9344f
iceice666 <iceice666@outlook.com>
2024-08-18 10:14:15 +0800

    add macro mapping desc

--- lua/plugins/editor/neo-composer.lua
+++ lua/plugins/editor/neo-composer.lua
@@ -5,13 +5,65 @@ return {
   event = "VeryLazy",
   opts = {
     keymaps = {
-      play_macro = "Q",
-      yank_macro = "yq",
-      stop_macro = "cq",
-      toggle_record = "q",
-      cycle_next = "<m-q>]",
-      cycle_prev = "<m-q>[",
-      toggle_macro_menu = "<c-q>",
+      play_macro = false,
+      yank_macro = false,
+      stop_macro = false,
+      toggle_record = false,
+      cycle_next = false,
+      cycle_prev = false,
+      toggle_macro_menu = false,
     },
   },
+  keys = {
+    group = "Macro",
+    {
+      "q",
+      function()
+        require("NeoComposer.macro").toggle_record()
+      end,
+      desc = "Record macro"
+    },
+    {
+      "Q",
+      function()
+        require("NeoComposer.macro").toggle_play_macro()
+      end,
+      desc = "Play macro"
+    },
+    {
+      "<m-q>]",
+      function()
+        require("NeoComposer.ui").cycle_next()
+      end,
+      desc = "Cycle next macro"
+    },
+    {
+      "<m-q>[",
+      function()
+        require("NeoComposer.ui").cycle_prev()
+      end,
+      desc = "Cycle prev macro"
+    },
+    {
+      "<c-q>",
+      function()
+        require("NeoComposer.ui").toggle_macro_menu()
+      end,
+      desc = "Toggle macro menu"
+    },
+    {
+      "<m-q>y",
+      function()
+        require("NeoComposer.macro").yank_macro()
+      end,
+      desc = "Yank macro"
+    },
+    {
+      "<m-q>s",
+      function()
+        require("NeoComposer.macro").stop_macro()
+      end,
+      desc = "Stop macro"
+    }
+  }
 }
